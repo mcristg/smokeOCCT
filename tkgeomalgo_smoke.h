c58491c074212a2652f78af9ea0508d0d7d1f194
mcristg <mcristernag@gmail.com>
2023-09-23 17:35:40 -0700

    Add TKGeomAlgo Toolkit

---
+++ tkgeomalgo_smoke.h
@@ -0,0 +1,16 @@
+#ifndef TKGEOMALGO_SMOKE_H
+#define TKGEOMALGO_SMOKE_H
+
+#include <smoke.h>
+
+// Defined in smokedata.cpp, initialized by init_tkgeomalgo_Smoke(), used by all .cpp files
+extern "C" SMOKE_EXPORT Smoke* tkgeomalgo_Smoke;
+extern "C" SMOKE_EXPORT void init_tkgeomalgo_Smoke();
+extern "C" SMOKE_EXPORT void delete_tkgeomalgo_Smoke();
+
+#ifndef QGLOBALSPACE_CLASS
+#define QGLOBALSPACE_CLASS
+class QGlobalSpace { };
+#endif
+
+#endif
