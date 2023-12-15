31693a858b8236607f22c236983acd03e4ac1984
mcristg <mcristernag@gmail.com>
2023-12-14 23:44:42 -0700

    Add TKSTL Toolkit

---
+++ tkstl/tkstl_includes.h
@@ -0,0 +1,9 @@
+#include <RWStl.hxx>
+#include <RWStl_ConfigurationNode.hxx>
+#include <RWStl_Provider.hxx>
+#include <RWStl_Reader.hxx>
+#include <StlAPI.hxx>
+#include <StlAPI_Reader.hxx>
+#include <StlAPI_Writer.hxx>
+
+
