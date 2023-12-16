83d04bef90e69b3d8afb2306c54201a9057d9b21
mcristg <mcristernag@gmail.com>
2023-12-16 14:26:40 -0700

    Add TKXDESTEP Toolkit

---
+++ tkxdestep/tkxdestep_includes.h
@@ -0,0 +1,21 @@
+#include <STEPCAFControl_ActorWrite.hxx>
+#include <STEPCAFControl_ConfigurationNode.hxx>
+#include <STEPCAFControl_Controller.hxx>
+#include <STEPCAFControl_DataMapIteratorOfDataMapOfLabelExternFile.hxx>
+#include <STEPCAFControl_DataMapIteratorOfDataMapOfLabelShape.hxx>
+#include <STEPCAFControl_DataMapIteratorOfDataMapOfPDExternFile.hxx>
+#include <STEPCAFControl_DataMapIteratorOfDataMapOfSDRExternFile.hxx>
+#include <STEPCAFControl_DataMapIteratorOfDataMapOfShapePD.hxx>
+#include <STEPCAFControl_DataMapIteratorOfDataMapOfShapeSDR.hxx>
+#include <STEPCAFControl_DataMapOfLabelExternFile.hxx>
+#include <STEPCAFControl_DataMapOfLabelShape.hxx>
+#include <STEPCAFControl_DataMapOfPDExternFile.hxx>
+#include <STEPCAFControl_DataMapOfSDRExternFile.hxx>
+#include <STEPCAFControl_DataMapOfShapePD.hxx>
+#include <STEPCAFControl_DataMapOfShapeSDR.hxx>
+#include <STEPCAFControl_ExternFile.hxx>
+#include <STEPCAFControl_GDTProperty.hxx>
+#include <STEPCAFControl_Provider.hxx>
+#include <STEPCAFControl_Reader.hxx>
+#include <STEPCAFControl_Writer.hxx>
+
