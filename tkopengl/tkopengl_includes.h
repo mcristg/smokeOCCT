52784ae94b0c5c8a683e13cee5af611165da25c2
mcristg <mcristernag@gmail.com>
2024-08-01 23:35:28 -0700

    Add wrapper class for OpenGl_Caps data fields

--- tkopengl/tkopengl_includes.h
+++ tkopengl/tkopengl_includes.h
@@ -114,3 +114,150 @@ DEFINE_HANDLECLASS(OpenGl_View)
 DEFINE_HANDLECLASS(OpenGl_Window)
 DEFINE_HANDLECLASS(OpenGl_Workspace)
 
+// OpenGl_Caps exposes its data fields (they are public) without using a set method, in CommonOCCT they
+// would be read-only and could not be modified. A wrapper class is then used to manage these data fields.
+
+class Set_OpenGl_Caps_Data_Fields {
+public: //! @name flags to disable particular functionality, should be used only for testing purposes!
+  static Standard_Boolean sRGBDisable(OpenGl_Caps &caps, Standard_Boolean sRGB_Disable)
+  { 
+    caps.sRGBDisable = sRGB_Disable;
+    return caps.sRGBDisable;
+  }
+  
+  static Standard_Boolean compressedTexturesDisable(OpenGl_Caps &caps, Standard_Boolean compressedTexturesDisable)
+  {
+    caps.compressedTexturesDisable = compressedTexturesDisable;
+    return caps.compressedTexturesDisable;
+  }
+
+  static Standard_Boolean vboDisable(OpenGl_Caps &caps,  Standard_Boolean vboDisable)
+  {
+    caps.vboDisable = vboDisable;
+    return caps.vboDisable;
+  }
+  
+  static Standard_Boolean pntSpritesDisable(OpenGl_Caps &caps, Standard_Boolean pntSpritesDisable)
+  {
+    caps.pntSpritesDisable = pntSpritesDisable;
+    return caps.pntSpritesDisable;
+  }
+
+  static Standard_Boolean keepArrayData(OpenGl_Caps &caps, Standard_Boolean keepArrayData)
+  {
+    caps.keepArrayData = keepArrayData;
+    return caps.keepArrayData;
+  }
+
+  static Standard_Boolean ffpEnable(OpenGl_Caps &caps, Standard_Boolean ffpEnable)
+  {
+    caps.ffpEnable = ffpEnable;
+    return caps.ffpEnable;
+  }
+
+  static Standard_Boolean usePolygonMode(OpenGl_Caps &caps, Standard_Boolean usePolygonMode)
+  {
+    caps.usePolygonMode = usePolygonMode;
+    return caps.usePolygonMode;
+  }
+
+  static Standard_Boolean useSystemBuffer(OpenGl_Caps &caps, Standard_Boolean useSystemBuffer)
+  {
+    caps.useSystemBuffer = useSystemBuffer;
+    return caps.useSystemBuffer;
+  }
+
+  static Standard_Integer swapInterval(OpenGl_Caps &caps, Standard_Integer swapInterval)
+  {
+    caps.swapInterval = swapInterval;
+    return caps.swapInterval;
+  }
+
+  static Standard_Boolean useZeroToOneDepth(OpenGl_Caps &caps, Standard_Boolean useZeroToOneDepth)
+  {
+    caps.useZeroToOneDepth = useZeroToOneDepth;
+    return caps.useZeroToOneDepth;
+  }
+
+  static Standard_Boolean buffersNoSwap(OpenGl_Caps &caps, Standard_Boolean buffersNoSwap)
+  {
+    caps.buffersNoSwap = buffersNoSwap;
+    return caps.buffersNoSwap;
+  }
+  static Standard_Boolean buffersOpaqueAlpha(OpenGl_Caps &caps, Standard_Boolean buffersOpaqueAlpha)
+  {
+    caps.buffersOpaqueAlpha = buffersOpaqueAlpha;
+    return caps.buffersOpaqueAlpha;
+  }
+
+  static Standard_Boolean buffersDeepColor(OpenGl_Caps &caps, Standard_Boolean buffersDeepColor)
+  {
+    caps.buffersDeepColor = buffersDeepColor;
+    return caps.buffersDeepColor;
+  }
+
+  static Standard_Boolean contextStereo(OpenGl_Caps &caps, Standard_Boolean contextStereo)
+  {
+    caps.contextStereo = contextStereo;
+    return caps.contextStereo;
+  }
+
+  static Standard_Boolean contextDebug(OpenGl_Caps &caps, Standard_Boolean contextDebug)
+  {
+    caps.contextDebug = contextDebug;
+    return caps.contextDebug;
+  }
+
+  static Standard_Boolean contextSyncDebug(OpenGl_Caps &caps, Standard_Boolean contextSyncDebug)
+  {
+    caps.contextSyncDebug = contextSyncDebug;
+    return caps.contextSyncDebug;
+  }
+
+  static Standard_Boolean contextNoAccel(OpenGl_Caps &caps, Standard_Boolean contextNoAccel)
+  {
+    caps.contextNoAccel = contextNoAccel;
+    return caps.contextNoAccel;
+  }
+
+  static Standard_Boolean contextCompatible(OpenGl_Caps &caps, Standard_Boolean contextCompatible)
+  {
+    caps.contextCompatible = contextCompatible;
+    return caps.contextCompatible;
+  }
+
+  static Standard_Boolean contextNoExtensions(OpenGl_Caps &caps, Standard_Boolean contextNoExtensions)
+  {
+    caps.contextNoExtensions = contextNoExtensions;
+    return caps.contextNoExtensions;
+  }
+
+  static Standard_Boolean isTopDownTextureUV(OpenGl_Caps &caps, Standard_Boolean isTopDownTextureUV)
+  {
+    caps.isTopDownTextureUV = isTopDownTextureUV;
+    return caps.isTopDownTextureUV;
+  }
+
+  static Standard_Boolean glslWarnings(OpenGl_Caps &caps, Standard_Boolean glslWarnings)
+  {
+    caps.glslWarnings = glslWarnings;
+    return caps.glslWarnings;
+  }
+
+  static Standard_Boolean suppressExtraMsg(OpenGl_Caps &caps, Standard_Boolean suppressExtraMsg)
+  {
+    caps.suppressExtraMsg = suppressExtraMsg;
+    return caps.suppressExtraMsg;
+  }
+
+  static OpenGl_ShaderProgramDumpLevel glslDumpLevel(OpenGl_Caps &caps, OpenGl_ShaderProgramDumpLevel glslDumpLevel)
+  {
+    caps.glslDumpLevel = glslDumpLevel;
+    return caps.glslDumpLevel;
+  }
+
+private:
+  //! Not implemented
+  Set_OpenGl_Caps_Data_Fields (const Set_OpenGl_Caps_Data_Fields& );
+  ~Set_OpenGl_Caps_Data_Fields ();
+};
